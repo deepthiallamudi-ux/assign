454dac1b51684109ffe6ca4d8952252d39322a04
deepthiallamudi-ux <deepthiallamudi@gmail.com>
2026-02-22 16:34:19 +0530

    add

--- q4.js
+++ q4.js
@@ -12,3 +12,10 @@ function runSequential(tasks, delay) {
         , Promise.resolve()).then(() => results);
     }
 
+runSequential([
+    () => Promise.resolve("Task 1 completed"),
+    () => Promise.resolve("Task 2 completed"),
+    () => Promise.resolve("Task 3 completed")
+], 1000).then(results => console.log(results));
+
+return results;
